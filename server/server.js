116b335990a96199dd99ab922e36bd586c7f462e
JHE <ajhe@study-area.org>
2018-08-10 10:45:44 +0800

    auto build

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var http=require('http');
 http.createServer(function(request,response){
 	response.writeHead(200, {'Content-Type': 'text/plain'})
-	response.write('Hello!\n');
+	response.write('Hello!!\n');
 	response.end();
 }).listen(3000);
 console.log('Server running')
